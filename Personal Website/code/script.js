40e9185b70b2f7127c0dfde465e50fd348b72aee
Illusioner <tomo701may@gmail.com>
2025-07-23 00:58:34 -0400

    Add all dialogue options in about page
    I figured out how to make the text boxes change when I press the a elements by making them use onclick instead of href, then I just used js to toggle showing and hiding the right elements. Next I'm going to make the door functionality I think, so I'll probably also make a couple new webpages for destinations. This time I'm going to add the info first and worry about formatting them later, as it gets tedious trying to quickly make drafts of what I need on ms paint and to set it up on the page correctly, and I need to focus on just figuring out how I'm going to make the doors work seamlessly.

--- Personal Website/code/script.js
+++ Personal Website/code/script.js
@@ -1,3 +1,53 @@
-function goto_About() {
-    window.location.href="about.html";
+function fisheyeMove(){
+    var x = document.getElementById("zoomOne");
+    var y = document.getElementById("zoomTwo");
+    var q = document.getElementById("boxOne");
+    var p = document.getElementById("boxTwo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        q.style.display = "block";
+        p.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+        q.style.display = "none";
+        p.style.display = "block";
+    }
+}
+
+function fisheyeMoveTwo(){
+    var x = document.getElementById("zoomOne");
+    var y = document.getElementById("zoomTwo");
+    var q = document.getElementById("boxOne");
+    var p = document.getElementById("boxThree");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        q.style.display = "block";
+        p.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+        q.style.display = "none";
+        p.style.display = "block";
+    }
+}
+
+function fisheyeMoveThree(){
+    var x = document.getElementById("zoomOne");
+    var y = document.getElementById("zoomTwo");
+    var q = document.getElementById("boxOne");
+    var p = document.getElementById("boxFour");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        q.style.display = "block";
+        p.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+        q.style.display = "none";
+        p.style.display = "block";
+    }
 }
