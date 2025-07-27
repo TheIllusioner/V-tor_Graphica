f984e32dac86c57dc87f9d7056a8e271c3815933
Illusioner <tomo701may@gmail.com>
2025-07-27 01:57:30 -0400

    add train stop (incomplete)
    I made it so that the train animations (the shaking and moving background) stop once the name of the station is clicked on, and it registers the different stations through a single function, but there are a lot of bugs and issues. The animation is made to first increase the animation duration after pressing the name of the station so that it slows down before stopping, and that works for the first station, but it doesn't work for any of the others. Instead, it just pauses instantly and instantly changes stations too. I feel like it might have something to do with the event listener. Once I figure out how to fix that I'll add some blank webpages for actually going to the stations and duplicate the navigation code onto the right door as I planned. I'll work on the doors opening later since I think I'll make a gif of that or something and add it in there and I just want to get most of the code done for now.

--- Personal Website/code/script.js
+++ Personal Website/code/script.js
@@ -1,4 +1,4 @@
-function fisheyeMove(){
+function fisheyeMove(){ //combine the first 3 functions into 1 eventually
     var x = document.getElementById("zoomOne");
     var y = document.getElementById("zoomTwo");
     var q = document.getElementById("boxOne");
@@ -49,5 +49,82 @@ function fisheyeMoveThree(){
         y.style.display = "block";
         q.style.display = "none";
         p.style.display = "block";
+    }
+}
+
+function nextStop(){
+    const stops = [document.getElementById("stopOne"), document.getElementById("stopTwo"), document.getElementById("stopThree")];
+    for(let j = 0; j < stops.length; j++)
+    {
+        console.log("outer " + j);
+        if(stops[j].style.display === "block" && j < stops.length-1)
+        {
+            stops[j].style.display = "none";
+            stops[j+1].style.display = "block";
+            console.log(j);
+            break;
+        } else if(j === stops.length-1)
+        {
+            stops[j].style.display = "none";
+            stops[0].style.display = "block";
+            console.log(j);
+            break;
+        }
+    }
+}
+
+function prevStop(){
+    const stops = [document.getElementById("stopOne"), document.getElementById("stopTwo"), document.getElementById("stopThree")];
+    const lastStop = stops.length-1;
+    for(let i = stops.length-1; i >= 0; i--)
+    {
+        console.log(i);
+        if(stops[i].style.display === "block" && i > 0)
+        {
+            stops[i].style.display = "none";
+            stops[i-1].style.display = "block";
+            console.log(i);
+            break;
+        } else if(i === 0)
+        {
+            stops[i].style.display = "none";
+            stops[lastStop].style.display = "block";
+            console.log(i);
+            break;
+        }
+    }
+}
+
+function stopTrain(stopNum){
+    const trainIn = document.getElementsByClassName("trainInside");
+    const trainOut = document.getElementById("bg");
+    trainOut.style.backgroundImage = "url('')";
+    trainOut.style.animationDuration = "200000s";
+    //console.log(trainIn);
+    for(let el of trainIn){
+        //console.log(el);
+        el.style.animationDuration = "5s";
+        if(el.style.animationDuration = "5s")
+        {
+            el.addEventListener("animationiteration", () =>{
+                el.style.animationPlayState = "paused";
+                trainOut.style.animationPlayState = "paused";
+                if(stopNum === '1')
+                {
+                    trainOut.style.backgroundColor = "#000000";
+                    console.log("stop one");
+                }
+                if(stopNum === '2')
+                {
+                    trainOut.style.backgroundColor = "#7f00adff";
+                    console.log("stop two");
+                }
+                if(stopNum === '3')
+                {
+                    trainOut.style.backgroundColor = "#0000ff";
+                    console.log("stop three");
+                }
+            });
+        }
     }
 }
