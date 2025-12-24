4ac58faff627cf8e473503d97c39dab92f54ab0e
Illusioner <tomo701may@gmail.com>
2025-12-24 00:35:51 -0500

    Add minor adjustments to text, complete navigation system
    I adjusted the text in the about section to be a little easier to understand, and made navigating between the different dialogue options controlled by 1 function instead of 3 to make it more efficient. Similarly, I improved the efficiency of the navigation system in the part where you change the target station by having just one a element where clicking the arrows changes the innerHTML to the correct station name. I had to make these changes to make it easier to copy the navigation functionality to the other set of doors on the right.
    
    I also got rid of the slowing to a stop thing that I was trying to do with the train when the target station was chosen. I couldn't figure out how to make it work consistently and I realized it would also be a little annoying to deal with when navigating the website anyways. So at this point I have the functionality of the homepage complete, though it will always be open to changes later.

--- Personal Website/code/script.js
+++ Personal Website/code/script.js
@@ -1,8 +1,8 @@
-function fisheyeMove() { //combine the first 3 functions into 1 eventually
+function fisheyeMove(diagOption) {
     var x = document.getElementById("zoomOne");
     var y = document.getElementById("zoomTwo");
     var q = document.getElementById("boxOne");
-    var p = document.getElementById("boxTwo");
+    var p = document.getElementById(diagOption);
     if (x.style.display === "none") {
         x.style.display = "block";
         y.style.display = "none";
@@ -16,138 +16,97 @@ function fisheyeMove() { //combine the first 3 functions into 1 eventually
     }
 }
 
-function fisheyeMoveTwo() {
-    var x = document.getElementById("zoomOne");
-    var y = document.getElementById("zoomTwo");
-    var q = document.getElementById("boxOne");
-    var p = document.getElementById("boxThree");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "none";
-        q.style.display = "block";
-        p.style.display = "none";
-    } else {
-        x.style.display = "none";
-        y.style.display = "block";
-        q.style.display = "none";
-        p.style.display = "block";
-    }
-}
-
-function fisheyeMoveThree() {
-    var x = document.getElementById("zoomOne");
-    var y = document.getElementById("zoomTwo");
-    var q = document.getElementById("boxOne");
-    var p = document.getElementById("boxFour");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "none";
-        q.style.display = "block";
-        p.style.display = "none";
-    } else {
-        x.style.display = "none";
-        y.style.display = "block";
-        q.style.display = "none";
-        p.style.display = "block";
-    }
-}
+const stops = ["Next Stop: V-Tor Graphica / Station Center", "Next Stop: V-Tor Graphica / Void Beach", "Next Stop: Proxia / Sector 5 Autostine Tech Station-A"];
+const stop = document.getElementsByClassName("stop");
 
 function nextStop() {
-    const stops = [document.getElementById("stopOne"), document.getElementById("stopTwo"), document.getElementById("stopThree")];
-    for(let j = 0; j < stops.length; j++)
-    {
-        console.log("outer " + j);
-        if(stops[j].style.display === "block" && j < stops.length-1)
-        {
-            stops[j].style.display = "none";
-            stops[j+1].style.display = "block";
-            console.log(j);
-            break;
-        } else if(j === stops.length-1)
-        {
-            stops[j].style.display = "none";
-            stops[0].style.display = "block";
-            console.log(j);
-            break;
+    stopChanged = false;
+    j = 0;
+    while(!stopChanged){
+        if(j < stops.length-1 && stop[0].innerHTML === stops[j]){
+            stop[0].innerHTML = stops[j+1];
+            stop[1].innerHTML = stops[j+1];
+            stopChanged = true;
         }
+        else if (j === stops.length-1 && stop[0].innerHTML === stops[j]){
+            j = 0;
+            stop[0].innerHTML = stops[j];
+            stop[1].innerHTML = stops[j];
+            stopChanged = true;
+        }
+        j++;
     }
 }
 
 function prevStop() {
-    const stops = [document.getElementById("stopOne"), document.getElementById("stopTwo"), document.getElementById("stopThree")];
-    const lastStop = stops.length-1;
-    for(let i = stops.length-1; i >= 0; i--)
-    {
-        console.log(i);
-        if(stops[i].style.display === "block" && i > 0)
-        {
-            stops[i].style.display = "none";
-            stops[i-1].style.display = "block";
-            console.log(i);
-            break;
-        } else if(i === 0)
-        {
-            stops[i].style.display = "none";
-            stops[lastStop].style.display = "block";
-            console.log(i);
-            break;
+    stopChanged = false;
+    j = stops.length-1;
+    while(!stopChanged){
+        if(j > 0 && stop[0].innerHTML === stops[j]){
+            stop[0].innerHTML = stops[j-1];
+            stop[1].innerHTML = stops[j-1];
+            stopChanged = true;
         }
+        else if (j === 0 && stop[0].innerHTML === stops[j]){
+            j = stops.length-1;
+            stop[0].innerHTML = stops[j];
+            stop[1].innerHTML = stops[j];
+            stopChanged = true;
+        }
+        j--;
     }
 }
 
-function stopTrain(stopNum) {
+function stopTrain() {
     const trainIn = document.getElementsByClassName("trainInside");
     const trainOut = document.getElementById("bg");
     const doorsOpen = document.getElementById("doorsOpen");
     const doorsClosed = document.getElementById("doorsClosed");
     trainOut.style.backgroundImage = "url('')";
-    trainOut.style.animationDuration = "200000s";
     //console.log(trainIn);
     for(let el of trainIn){
-        //console.log(el);
-        el.style.animationDuration = "5s";
-        if(el.style.animationDuration = "5s")
+        el.style.animationPlayState = "paused";
+        trainOut.style.animationPlayState = "paused";
+        doorsOpen.style.display = "none";
+        doorsClosed.style.display = "block";
+        if(stop[0].innerHTML === stops[0])
         {
-            el.addEventListener("animationiteration", () =>{
-                el.style.animationPlayState = "paused";
-                trainOut.style.animationPlayState = "paused";
-                doorsOpen.style.display = "none";
-                doorsClosed.style.display = "block";
-                if(stopNum === '1')
-                {
-                    trainOut.style.backgroundColor = "#000000";
-                    goToStop('1');
-                    console.log("stop one");
-                }
-                if(stopNum === '2')
-                {
-                    trainOut.style.backgroundColor = "#7f00adff";
-                    goToStop('2');
-                    console.log("stop two");
-                }
-                if(stopNum === '3')
-                {
-                    trainOut.style.backgroundColor = "#0000ff";
-                    goToStop('3');
-                    console.log("stop three");
-                }
-            });
+            trainOut.style.backgroundColor = "#000000";
+            goToStop('1');
+            console.log("stop one");
+        }
+        if(stop[0].innerHTML === stops[1])
+        {
+            trainOut.style.backgroundColor = "#7f00adff";
+            goToStop('2');
+            console.log("stop two");
+        }
+        if(stop[0].innerHTML === stops[2])
+        {
+            trainOut.style.backgroundColor = "#0000ff";
+            goToStop('3');
+            console.log("stop three");
         }
     }
 }
 
 function goToStop(stopNum) {
-    const outside = document.getElementById("outsideLeft");
+    const outsideL = document.getElementById("outsideLeft");
+    const outsideR = document.getElementById("outsideRight");
+
     if(stopNum === '1')
     {
-        outside.href = "station_center.html";
+        outsideL.href = "station_center.html";
+        outsideR.href = "station_center.html";
     }
     if(stopNum === '2')
     {
-        outside.href = "void_beach.html";
+        outsideL.href = "void_beach.html";
+        outsideR.href = "void_beach.html";
     }
     if(stopNum === '3')
     {
-        outside.href = "sector_5_station_a.html";
+        outsideL.href = "sector_5_station_a.html";
+        outsideR.href = "sector_5_station_a.html";
     }
 }
